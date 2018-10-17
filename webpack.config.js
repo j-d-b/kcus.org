9e0fdf63eeb807f0cee017a15490e95b84b6bbfd
j-d-b <jacob@jdbrady.info>
2018-10-17 12:42:36 -0400

    10x faster builds and slightly smaller images

--- webpack.config.js
+++ webpack.config.js
@@ -3,60 +3,55 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const webpack = require('webpack');
 
-module.exports = (env, argv) => {
-  const plugins = [
+module.exports = (env, argv) => ({
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          babelrc: false
+        }
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new CopyWebpackPlugin([
       { from: './src/images', to: './images' },
       { from: './src/images/favicons/favicon.ico', to: './' },
       { from: './src/index.html', to: './' },
       { from: './src/netlify/_redirects', to: './' }
-    ])
-  ];
-
-  if (argv.mode === 'production') {
-    plugins.push(new ImageminPlugin({
+    ]),
+    new ImageminPlugin({
+      disable: argv.mode !== 'production',
       test: /\.(jpe?g|png|gif|svg)$/i,
-      optipng: {
-        optimizationLevel: 7
+      pngquant: {
+        speed: 10,
+        quality: '50-70'
       },
       jpegtran: {
         optimize: true
       }
-    }));
-  }
-
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-            babelrc: false
-          }
-        },
-        {
-          test: /\.hbs$/,
-          loader: 'handlebars-loader'
-        },
-        {
-          test: /\.scss$/,
-          loader: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    },
-    plugins
-  }
-};
+    })
+  ]
+});
