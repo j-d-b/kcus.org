52c892c644dde2e07f0cc1586fb316e8e3b19544
Jacob Brady <jacob@jdbrady.info>
2020-03-30 14:15:42 -0400

    chore: set placeholder page for kcus/ibi merge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 // Jacob Brady
-// 2018 KCUS Inc.
+// 2020 KCUS Inc.
 import 'nodelist-foreach-polyfill';
 import style from './styles/style.scss'; // single, global stylesheet
-import { route, setupNavigation } from './router.js';
-
-// setup the spa internal link handlers for the navbar, which never reloads
-setupNavigation(document.querySelector('nav'));
-
-// route the intial page, based on url
-route(window.location.pathname);
-
-// route the corresponding page when the user does a browser back or forward
-window.onpopstate = () => route(window.location.pathname);
